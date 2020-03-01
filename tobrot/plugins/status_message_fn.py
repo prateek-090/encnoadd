a88067f3528f3934baa6b8c5fd2053892e44516c
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-03-01 15:29:48 +0530

    Update status_message_fn.py

--- tobrot/plugins/status_message_fn.py
+++ tobrot/plugins/status_message_fn.py
@@ -12,8 +12,11 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 LOGGER = logging.getLogger(__name__)
 
 import asyncio
+import inspect
 import os
 import time
+import sys
+import traceback
 
 from tobrot import (
     MAX_MESSAGE_LENGTH
@@ -136,7 +139,65 @@ async def exec_message_f(client, message):
             await message.delete()
         else:
             await message.reply_text(OUTPUT)
+            
+async def eval(client, message):
+    await message.edit("Processing ...")
+    cmd = message.text.split(" ", maxsplit=1)[1]
 
+    reply_to_id = message.message_id
+    if message.reply_to_message:
+        reply_to_id = message.reply_to_message.message_id
+
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = io.StringIO()
+    redirected_error = sys.stderr = io.StringIO()
+    stdout, stderr, exc = None, None, None
+
+    try:
+        await aexec(cmd, client, message)
+    except Exception:
+        exc = traceback.format_exc()
+
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
+    else:
+        evaluation = "Success"
+
+    final_output = "**EVAL**: ```{}```\n\n**OUTPUT**:\n```{}``` \n".format(cmd, evaluation.strip())
+
+    if len(final_output) > MAX_MESSAGE_LENGTH:
+        with open("eval.text", "w+", encoding="utf8") as out_file:
+            out_file.write(str(final_output))
+        await client.send_document(
+            chat_id=message.chat.id,
+            document="eval.text",
+            caption=cmd,
+            disable_notification=True,
+            reply_to_message_id=reply_to_id
+        )
+        os.remove("eval.text")
+        await message.delete()
+    else:
+        await message.edit(final_output)
+
+
+async def aexec(code, client, message):
+    exec(
+        f'async def __aexec(client, message): ' +
+        ''.join(f'\n {l}' for l in code.split('\n'))
+    )
+    return await locals()['__aexec'](client, message)
 
 async def upload_document_f(client, message):
     imsegd = await message.reply_text(
