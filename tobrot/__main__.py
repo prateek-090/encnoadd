c72863d74ad59ebc31238fb2b80a4bcf00029cc9
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-08-25 20:55:39 +0530

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -32,7 +32,14 @@ from tobrot import (
     Clear_thumb_CMD_TRIGGER
 )
 
-from pyrogram import Client, Filters, MessageHandler, CallbackQueryHandler
+from pyrogram import (
+    Client,
+    filters
+)
+from pyrogram.handlers import (
+    MessageHandler,
+    CallbackQueryHandler
+)
 
 from tobrot.plugins.new_join_fn import new_join_f, help_message_f, rename_message_f
 from tobrot.plugins.incoming_message_fn import incoming_message_f, incoming_youtube_dl_f
@@ -65,67 +72,67 @@ if __name__ == "__main__" :
     #
     incoming_message_handler = MessageHandler(
         incoming_message_f,
-        filters=Filters.command([Leech_CMD_TRIGGER]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=filters.command([Leech_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(incoming_message_handler)
     #
     incoming_youtube_dl_handler = MessageHandler(
         incoming_youtube_dl_f,
-        filters=Filters.command([Ytdl_CMD_TRIGGER]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=filters.command([Ytdl_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(incoming_youtube_dl_handler)
     #
     status_message_handler = MessageHandler(
         status_message_f,
-        filters=Filters.command([Status_CMD_TRIGGER]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=filters.command([Status_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(status_message_handler)
     #
     cancel_message_handler = MessageHandler(
         cancel_message_f,
-        filters=Filters.command([Cancel_CMD_TRIGGER]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=filters.command([Cancel_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(cancel_message_handler)
     #
     exec_message_handler = MessageHandler(
         exec_message_f,
-        filters=Filters.command([EXEC_CMD_TRIGGER]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=filters.command([EXEC_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(exec_message_handler)
     #
     eval_message_handler = MessageHandler(
         eval_message_f,
-        filters=Filters.command([Eval_CMD_TRIGGER]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=filters.command([Eval_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(eval_message_handler)
     #
     rename_message_handler = MessageHandler(
         rename_message_f,
-        filters=Filters.command([Rename_CMD_TRIGGER]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=filters.command([Rename_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(rename_message_handler)
     #
     upload_document_handler = MessageHandler(
         upload_document_f,
-        filters=Filters.command([Upload_CMD_TRIGGER]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=filters.command([Upload_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(upload_document_handler)
 
     help_text_handler = MessageHandler(
         help_message_f,
-        filters=Filters.command(["help"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=filters.command(["help"]) & filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(help_text_handler)
     #
     new_join_handler = MessageHandler(
         new_join_f,
-        filters=~Filters.chat(chats=AUTH_CHANNEL)
+        filters=~filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(new_join_handler)
     #
     group_new_join_handler = MessageHandler(
         help_message_f,
-        filters=Filters.chat(chats=AUTH_CHANNEL) & Filters.new_chat_members
+        filters=filters.chat(chats=AUTH_CHANNEL) & filters.new_chat_members
     )
     app.add_handler(group_new_join_handler)
     #
@@ -136,13 +143,13 @@ if __name__ == "__main__" :
     #
     save_thumb_nail_handler = MessageHandler(
         save_thumb_nail,
-        filters=Filters.command([Save_Thumb_CMD_TRIGGER]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=filters.command([Save_Thumb_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(save_thumb_nail_handler)
     #
     clear_thumb_nail_handler = MessageHandler(
         clear_thumb_nail,
-        filters=Filters.command([Clear_thumb_CMD_TRIGGER]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=filters.command([Clear_thumb_CMD_TRIGGER]) & filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(clear_thumb_nail_handler)
     #
