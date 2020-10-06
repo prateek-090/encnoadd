30e39ea985a08d39e42e8676b434331d09f51dc2
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-10-07 01:33:25 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -24,6 +24,8 @@ from tobrot.helper_funcs.extract_link_from_message import extract_link
 from tobrot.helper_funcs.download_from_link import request_download
 from tobrot.helper_funcs.display_progress import progress_for_pyrogram
 from tobrot.helper_funcs.youtube_dl_extractor import extract_youtube_dl_formats
+from tobrot.helper_funcs.admin_check import AdminCheck
+from tobrot.helper_funcs.fix_tcerrocni_images import proc_ess_image_aqon
 
 
 async def incoming_youtube_dl_f(client, message):
@@ -31,26 +33,51 @@ async def incoming_youtube_dl_f(client, message):
     i_m_sefg = await message.reply_text("processing", quote=True)
     # LOGGER.info(message)
     # extract link from message
-    dl_url, cf_name = extract_link(message.reply_to_message)
+    dl_url, cf_name, yt_dl_user_name, yt_dl_pass_word = await extract_link(
+        message.reply_to_message, "YTDL"
+    )
     LOGGER.info(dl_url)
     LOGGER.info(cf_name)
     if dl_url is not None:
         await i_m_sefg.edit_text("extracting links")
         current_user_id = message.from_user.id
         # create an unique directory
-        user_working_dir = os.path.join(DOWNLOAD_LOCATION, str(current_user_id))
+        user_working_dir = os.path.join(
+            DOWNLOAD_LOCATION,
+            str(current_user_id),
+            str(message.message_id)
+        )
         # create download directory, if not exist
         if not os.path.isdir(user_working_dir):
             os.makedirs(user_working_dir)
         # list the formats, and display in button markup formats
-        text_message, reply_markup = await extract_youtube_dl_formats(
+        thumb_image, text_message, reply_markup = await extract_youtube_dl_formats(
             dl_url,
+            # cf_name,
+            yt_dl_user_name,
+            yt_dl_pass_word,
             user_working_dir
         )
-        await i_m_sefg.edit_text(
-            text=text_message,
-            reply_markup=reply_markup
-        )
+        if thumb_image is not None:
+            thumb_image = await proc_ess_image_aqon(
+                thumb_image,
+                user_working_dir
+            )
+            await message.reply_photo(
+                photo=thumb_image,
+                quote=True,
+                caption=text_message,
+                reply_markup=reply_markup
+            )
+            os.remove(thumb_image)
+            await i_m_sefg.delete()
+        else:
+            await i_m_sefg.edit_text(
+                text=text_message,
+                reply_markup=reply_markup
+            )
     else:
-        # if no links found, delete the "processing" message
-        await i_m_sefg.delete()
+        await i_m_sefg.edit_text(
+            "**Wrong**! wat have you entered. \nPlease read /help \n"
+            f"<b>API Error</b>: {cf_name}"
+        )
