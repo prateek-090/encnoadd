bb1e83fcecaa60bf12b8b37463f376ed49de36dc
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-10-07 01:26:21 +0530

    Update call_back_button_handler.py

--- tobrot/plugins/call_back_button_handler.py
+++ tobrot/plugins/call_back_button_handler.py
@@ -4,16 +4,33 @@
 
 # the logging things
 import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
 LOGGER = logging.getLogger(__name__)
 
-
+from pyrogram.types import CallbackQuery
 from tobrot.helper_funcs.youtube_dl_button import youtube_dl_call_back
+from tobrot.helper_funcs.icntaosrtsba import (
+    leech_btn_k,
+    ytdl_btn_k
+)
+from tobrot import (
+    Ytdl_CMD_TRIGGER
+)
 
 
-async def button(bot, update):
-    # LOGGER.info(update)
+async def button(bot, update: CallbackQuery):
+    LOGGER.info(update)
+    if update.from_user.id != update.message.reply_to_message.from_user.id:
+        return
+
+    await update.answer()
     cb_data = update.data
-    if "|" in cb_data:
+    
+    if cb_data.startswith(Ytdl_CMD_TRIGGER):
+        await ytdl_btn_k(update.message)
+
+    elif "|" in cb_data:
         await youtube_dl_call_back(bot, update)
