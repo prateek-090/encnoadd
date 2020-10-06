3e65bf850e5e111ad2a5651c115112e7bd913495
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-10-06 22:18:15 +0530

    Update download.py

--- tobrot/helper_funcs/download.py
+++ tobrot/helper_funcs/download.py
@@ -1,1 +1,55 @@
-.
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K
+
+# the logging things
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+LOGGER = logging.getLogger(__name__)
+
+import io
+import asyncio
+import inspect
+import os
+import time
+import sys
+import traceback
+
+from tobrot import (
+    MAX_MESSAGE_LENGTH
+)
+
+
+async def down_load_media_f(client, message):
+    user_id = message.from_user.id
+    print(user_id)
+    mess_age = await message.reply_text("...", quote=True)
+    if not os.path.isdir(DOWNLOAD_LOCATION):
+        os.makedirs(DOWNLOAD_LOCATION)
+    if message.reply_to_message is not None:
+        start_t = datetime.now()
+        download_location = "/app/"
+        c_time = time.time()
+        the_real_download_location = await client.download_media(
+            message=message.reply_to_message,
+            file_name=download_location,
+            progress=progress_for_pyrogram_g,
+            progress_args=(
+                "trying to download", mess_age, c_time
+            )
+        )
+        end_t = datetime.now()
+        ms = (end_t - start_t).seconds
+        LOGGER.info(the_real_download_location)
+        await asyncio.sleep(10)
+        await mess_age.edit_text(f"Downloaded to <code>{the_real_download_location}</code> in <u>{ms}</u> seconds")
+        the_real_download_location_g = os.path.basename(the_real_download_location)
+        LOGGER.info(the_real_download_location_g)
+
+    else:
+        #await asyncio.sleep(4)
+        await mess_age.edit_text("Reply to a Telegram Media, to save to the server.")
