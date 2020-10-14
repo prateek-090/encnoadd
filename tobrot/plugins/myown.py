90e1b866c476cc91013561c57f4dcb17325b5aaf
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-10-14 09:29:24 +0530

    Create myown.py

---
+++ tobrot/plugins/myown.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K
+
+# the logging things
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+LOGGER = logging.getLogger(__name__)
+
+
+import os
+
+from tobrot import (
+    DOWNLOAD_LOCATION
+)
+
+
+import time
+from tobrot.helper_funcs
