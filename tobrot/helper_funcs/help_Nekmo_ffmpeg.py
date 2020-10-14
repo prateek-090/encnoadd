ad23316ecf4ebbb8c9639337dce7d5919871e501
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-10-14 09:24:32 +0530

    Update help_Nekmo_ffmpeg.py

--- tobrot/helper_funcs/help_Nekmo_ffmpeg.py
+++ tobrot/helper_funcs/help_Nekmo_ffmpeg.py
@@ -51,3 +51,70 @@ async def take_screen_shot(video_file, output_directory, ttl):
         return out_put_file_name
     else:
         return None
+    
+    
+async def mux_video(video_file, sub_file, output_name):
+    # https://stackoverflow.com/a/13891070/4723940
+    new_name = os.path.basename(output_name)
+    output_directory = os.path.dirname(os.path.abspath(output_name))
+    out_put_file_name = os.path.join(output_directory, new_name)
+    if video_file.upper().endswith(("MKV", "MP4", "WEBM")):
+        file_genertor_command = [
+            "ffmpeg",
+            "-i",
+            video_file,
+            "-i",
+            sub_file,
+            "-c copy",
+            "-attach",
+            "cover.jpg",
+            "-metadata:s:t mimetype=image/jpeg -metadata:s:t filename=cover.jpg",
+            "-metadata title="Upl'd By Team-D&O @dramaost TG Group"",
+            out_put_file_name
+        ]
+        # width = "90"
+        process = await asyncio.create_subprocess_exec(
+            *file_genertor_command,
+            # stdout must a pipe to be accessible as process.stdout
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        # Wait for the subprocess to finish
+        stdout, stderr = await process.communicate()
+        e_response = stderr.decode().strip()
+        t_response = stdout.decode().strip()
+    #
+        LOGGER.info(out_put_file_name)
+        
+async def mux_do_video(video_file, sub_file, output_name):
+    # https://stackoverflow.com/a/13891070/4723940
+    new_name = os.path.basename(output_name)
+    output_directory = os.path.dirname(os.path.abspath(output_name))
+    out_put_file_name = os.path.join(output_directory, new_name)
+    if video_file.upper().endswith(("MKV", "MP4", "WEBM")):
+        file_genertor_command = [
+            "ffmpeg",
+            "-i",
+            video_file,
+            "-i",
+            sub_file,
+            "-c copy",
+            "-attach",
+            "Docover.jpg",
+            "-metadata:s:t mimetype=image/jpeg -metadata:s:t filename=cover.jpg",
+            "-metadata title="Upl'd By Team-D&O @dramaost TG Group"",
+            out_put_file_name
+        ]
+        # width = "90"
+        process = await asyncio.create_subprocess_exec(
+            *file_genertor_command,
+            # stdout must a pipe to be accessible as process.stdout
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        # Wait for the subprocess to finish
+        stdout, stderr = await process.communicate()
+        e_response = stderr.decode().strip()
+        t_response = stdout.decode().strip()
+    #
+        LOGGER.info(out_put_file_name)
